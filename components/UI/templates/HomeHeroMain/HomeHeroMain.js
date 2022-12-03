5f911c783f01507f0e78964836909eb7a536d40e
debdevs <debdevs97@gmail.com>
2022-12-03 18:57:00 -0500

    import poppins font
    import poppins font and apply to h1

--- components/UI/templates/HomeHeroMain/HomeHeroMain.js
+++ components/UI/templates/HomeHeroMain/HomeHeroMain.js
@@ -7,7 +7,15 @@ import styles from './HomeHeroMain.module.css'
 const HomeHeroMain = () => {
   return (
     <div className={styles.hero_container}>
-      <div className={`${styles.hero_content_container} ${"padding_base"}`}></div>
+      <div className={`${styles.hero_content_container} ${"padding_base"}`}>
+        <div className= {styles.text_container}>
+          <h1>Hello</h1>
+          <h2>Hello</h2>
+        </div>
+
+
+
+      </div>
        </div>
   )
 }
