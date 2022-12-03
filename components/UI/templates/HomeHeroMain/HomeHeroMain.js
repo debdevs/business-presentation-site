216e2fd048cec7235529a6a1ca63d8161c25e9be
debdevs <debdevs97@gmail.com>
2022-12-03 18:43:45 -0500

    create base component structure
    initialize project and create basic structure

---
+++ components/UI/templates/HomeHeroMain/HomeHeroMain.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './HomeHeroMain.module.css'
+
+
+
+const HomeHeroMain = () => {
+  return (
+    <div className={styles.hero_container}> HomeHeroMain</div>
+  )
+}
+
+export default HomeHeroMain
