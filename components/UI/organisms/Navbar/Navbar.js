1c07781af0737fb332e9bd5ea91d1f9fe9c47a0e
debdevs <debdevs97@gmail.com>
2022-12-04 00:22:37 -0500

    import logo to navbar

--- components/UI/organisms/Navbar/Navbar.js
+++ components/UI/organisms/Navbar/Navbar.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import styles from './Navbar.module.css'
+import svg from '../../../../public/logo.svg'
+import Image from 'next/image'
 const Navbar = () => {
   return (
-    <nav className={styles.navbar}>Navbar</nav>
+    <nav className={styles.navbar}>          
+        <nav className={styles.navbar_content}>
+     <div className={styles.navbar_image_wrapper}>
+    <Image
+    src="/logo.png"
+    alt="Picture of the author"
+    layout='fill'
+ 
+  /></div>Navbar</nav>      
+  
+  </nav>
   )
 }
 
