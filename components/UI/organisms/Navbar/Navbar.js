6398f156d328d45880355ae2bab5d7c909ac18ab
debdevs <debdevs97@gmail.com>
2022-12-04 00:59:46 -0500

    create base code for hamburger menu

--- components/UI/organisms/Navbar/Navbar.js
+++ components/UI/organisms/Navbar/Navbar.js
@@ -2,7 +2,14 @@ import React from 'react'
 import styles from './Navbar.module.css'
 import svg from '../../../../public/logo.svg'
 import Image from 'next/image'
+import { useState } from 'react'
+
+
 const Navbar = () => {
+    const [active, setActive] = useState(false)
+    const top_styles = [styles.top_bar, styles.top_bar_active]
+    const mid_styles = [styles.mid_bar, styles.mid_bar_active]
+    const bottom_styles = [styles.bottom_bar, styles.bottom_bar_active]
   return (
     <nav className={styles.navbar}>          
         <nav className={styles.navbar_content}>
@@ -12,7 +19,15 @@ const Navbar = () => {
     alt="Picture of the author"
     layout='fill'
  
-  /></div>Navbar</nav>      
+  /></div>
+
+
+  <ul onClick={() => {active == false ? setActive(true): setActive(false); console.log(9)}} className={styles.menu_parent}>
+    <li className={active == false ? top_styles[0]:top_styles[1]}></li>
+    <li className={active == false ? mid_styles[0]:mid_styles[1]}></li>
+    <li className={active == false ? bottom_styles[0]:bottom_styles[1]}></li>
+    </ul>
+    </nav>      
   
   </nav>
   )
