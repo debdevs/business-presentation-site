e5661a104845f6f797936f070c3579a0b9defa94
debdevs <debdevs97@gmail.com>
2022-12-03 20:25:22 -0500

    add base code for HomeHeroMain
    revise styling for HomeHeroMain

--- components/UI/templates/HomeHeroMain/HomeHeroMain.js
+++ components/UI/templates/HomeHeroMain/HomeHeroMain.js
@@ -2,28 +2,34 @@ import React from 'react'
 
 import styles from './HomeHeroMain.module.css'
 
-
+import Image from 'next/image'
+{/* <div className={`${styles.hero_content_container} ${"padding_base"}`}> */}
 
 const HomeHeroMain = () => {
   return (
     <div className={styles.hero_container}>
-      
-      <div className={`${styles.hero_content_container} ${"padding_base"}`}>
-      <div className= {styles.left_container}>
-        <div className= {styles.text_container}>
-          <h1>01</h1>
-          <div className= {styles.front_text_container}>
-            <h2>The Story.</h2>
-            <div className= {styles.lower_texts}>
-              <p>Discover how to partner with us to create amazing presentations</p>
-              <button className= {styles.plain_button} >Read More</button>
-            </div>
-          </div>  
-        </div>          
+      <div className= {styles.left_bg}/>
+      <div className= {styles.right_bg}/>
+      <div className={styles.hero_content_container}>
+        <div className={styles.left_content}>
+          <div className={styles.top_items}>
+            <h1>The Story.</h1>
+            <h2>01</h2>
+
+          </div>
+          <div className={styles.bottom_items}>
+            <p>FDSFDI AFHASO FHUIDS F</p>
+            <button>Read More</button>
+            
+          </div>
         </div>
-        <div className= {styles.right_container}></div>
-      </div>
+        <div className={styles.right_content}>
+          <Image></Image>
+        </div>
+
       
+      </div>
+
  
       </div>
   )
